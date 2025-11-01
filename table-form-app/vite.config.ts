0e2ccc0158ba6ded140e5001bef5f11ea22bbb49
sowmiya <iamsowmiya.rajkumar@gmail.com>
2025-11-01 11:01:54 +0530

    Initial commit

--- table-form-app/vite.config.ts
+++ table-form-app/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/YOUR_REPO_NAME/', // 👈 must match your GitHub repo name
   plugins: [react()],
-})
+});
